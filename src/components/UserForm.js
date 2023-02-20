3b6890e5fce390b9c3b5b90cd5ebf1e3f78f6a3a
katkola <katkola@gmail.com>
2023-02-20 00:37:00 -0500

    length validations

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -2,40 +2,122 @@ import React, { useState } from  'react';
 
 
 const UserForm = ( ) => {
-    const [title, setTitle] = useState("");
-    const [titleError, setTitleError] = useState("");
-    
-    const handleTitle = (e) => {
-        setTitle(e.target.value);
+    const [fname, setFname] = useState("");
+    const [fnameError, setFnameError] = useState("") ;
+    const [lname, setLname] = useState("");
+    const [lnameError, setLnameError] = useState("") ;
+    const [email, setEmail] = useState("");
+    const [emailError, setEmailError] = useState("");
+    const [password, setPassword] = useState("");  
+    const [passwordError, setPasswordError] = useState("");  
+
+    const handleLname = (e) => {
+        setLname(e.target.value);
         if(e.target.value.length < 1) {
-            setTitleError("Title is required!");
+            setLnameError("Title is required!");
         } else if(e.target.value.length < 3) {
-            setTitleError("Title must be 3 characters or longer!");
+            setLnameError("Title must be 3 characters or longer!");
         } else {
             // an empty string is considered a "falsy" value
-            setTitleError("");
+            setLnameError("");
         }
     }
+
+    const handleEmail = (e) => {
+        setEmail(e.target.value);
+        if(e.target.value.length < 1) {
+            setEmailError("Title is required!");
+        } else if(e.target.value.length < 3) {
+            setEmailError("Title must be 3 characters or longer!");
+        } else {
+            // an empty string is considered a "falsy" value
+            setEmailError("");
+        }
+    }
+
+    const handlePassword = (e) => {
+        setPassword(e.target.value);
+        if(e.target.value.length < 1) {
+            setPasswordError("Title is required!");
+        } else if(e.target.value.length < 3) {
+            setPasswordError("Title must be 3 characters or longer!");
+        } else {
+            // an empty string is considered a "falsy" value
+            setPasswordError("");
+        }
+    }
+
+    const handleFName = (e) => {
+        setFname(e.target.value);
+        if(e.target.value.length < 1) {
+            setFnameError("Title is required!");
+        } else if(e.target.value.length < 3) {
+            setFnameError("Title must be 3 characters or longer!");
+        } else {
+            // an empty string is considered a "falsy" value
+            setFnameError("");
+        }
+    }
+
+    const createUser = (e) => {
+        // we must prevent the default refresh of the browser to keep our state from being reset
+        e.preventDefault();
+    
+        // create a javascript object to hold all of the values
+        const newUser = { fname, email, password};
+        console.log("Welcome", newUser);
+    };
+
+    
     
     {/* this functionality can be repeated for each input needing validation */}
     
     return (
-        <form onSubmit={ (e) => e.preventDefault() }>
+        <form onSubmit={createUser}>
+            
             <div>
-                <label>Title: </label>
-                <input type="text" onChange={ handleTitle } />
+                <label>First Name: </label>
+                <input type="text" onChange={ handleFName } />
                 {
-                    titleError ?
-                    <p>{ titleError }</p> :
+                    fnameError ?
+                    <p>{ fnameError }</p> :
+                    ''
+                }
+            </div>
+            <div>
+                <label>Last Name: </label>
+                <input type="text" onChange={ handleLname } />
+                {
+                    lnameError&& lname.length>1 ?
+                    <p>{ lnameError }</p> :
+                    ''
+                }
+            </div>
+            <div>
+                <label>Email: </label>
+                <input type="text" onChange={ handleEmail } />
+                {
+                    emailError ?
+                    <p>{ emailError }</p> :
+                    ''
+                }
+            </div>
+            <div>
+                <label>Password: </label>
+                <input type="password" onChange={ handlePassword } />
+                {
+                    passwordError ?
+                    <p>{ passwordError }</p> :
                     ''
                 }
             </div>
             {
-                titleError ?
+                fnameError ?
                 <input type="submit" value="Create Movie" disabled /> : 
                 <input type="submit" value="Create Movie" />
             }
         </form>
+
     );
 }
 export default UserForm;
