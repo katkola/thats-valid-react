2301fd2fade138ce70c586618cec4b2a83785062
katkola <katkola@gmail.com>
2023-02-20 00:18:17 -0500

    reset with new template for conditionals

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,74 +1,41 @@
 import React, { useState } from  'react';
+
+
+const UserForm = ( ) => {
+    const [title, setTitle] = useState("");
+    const [titleError, setTitleError] = useState("");
     
-const UserForm = (props) => {
-    const [fname, setFname] = useState("");
-    const [lname, setLname] = useState("");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");  
-    const [confirmation, setConfirmation] = useState("");
-    const [hasBeenSubmitted, setHasBeenSubmitted] = useState(false);  
-
+    const handleTitle = (e) => {
+        setTitle(e.target.value);
+        if(e.target.value.length < 1) {
+            setTitleError("Title is required!");
+        } else if(e.target.value.length < 3) {
+            setTitleError("Title must be 3 characters or longer!");
+        } else {
+            // an empty string is considered a "falsy" value
+            setTitleError("");
+        }
+    }
     
-    const createUser = (e) => {
-        // we must prevent the default refresh of the browser to keep our state from being reset
-        e.preventDefault();
+    {/* this functionality can be repeated for each input needing validation */}
     
-        // create a javascript object to hold all of the values
-        const newUser = { fname, email, password, confirmation};
-        console.log("Welcome", newUser);
-        setFname("");
-        setLname("");
-    	setEmail("");
-    	setPassword("");
-
-        setHasBeenSubmitted( true );
-    };
-    const formMessage = () => {
-        if( hasBeenSubmitted ) {
-	    return "Thank you for submitting the form!";
-	} else {
-	    return "Welcome, please submit the form";
-	}
-    };
-
-
-    return(
-        <>
-            <form onSubmit={createUser}>
+    return (
+        <form onSubmit={ (e) => e.preventDefault() }>
+            <div>
+                <label>Title: </label>
+                <input type="text" onChange={ handleTitle } />
                 {
-                    hasBeenSubmitted
-                    ? <h3>Thank you for submitting the form!</h3>
-                    : <h3>Welcome, please submit the form.</h3>
+                    titleError ?
+                    <p>{ titleError }</p> :
+                    ''
                 }
-                <div>
-                    <label>first name: </label>
-                    <input type="text" onChange={(e) => setFname(e.target.value)} value={fname} />
-                </div>
-                <div>
-                    <label>last name: </label>
-                    <input type="text" onChange={(e) => setLname(e.target.value)} value={lname} />
-                </div>
-                <div>
-                    <label>Email Address: </label>
-                    <input type="text" onChange={(e) => setEmail(e.target.value)} value={email} />
-                </div>
-                <div>
-                    <label>Password: </label>
-                    <input type="text" onChange={(e) => setPassword(e.target.value)} value={password} />
-                </div>
-                <div>
-                    <label>Confirm Password: </label>
-                    <input type="text" onChange={(e) => setConfirmation(e.target.value)} value={confirmation} />
-                </div>
-                <input type="submit" value="Create User" />
-            </form>
-            <p>First Name: {fname}</p>
-            <p>Last Name: {lname}</p>
-            <p>Password: {password}</p>
-            <p>Email: {email}</p>
-            <p>Confirm Password: {confirmation}</p>
-        </>
+            </div>
+            {
+                titleError ?
+                <input type="submit" value="Create Movie" disabled /> : 
+                <input type="submit" value="Create Movie" />
+            }
+        </form>
     );
-};
-    
+}
 export default UserForm;
