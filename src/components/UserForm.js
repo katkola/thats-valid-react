2f5abb76c67b242cd0064bd615c80cb9030fd813
katkola <katkola@gmail.com>
2023-02-20 00:54:56 -0500

    all validations added

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -10,15 +10,16 @@ const UserForm = ( ) => {
     const [emailError, setEmailError] = useState("");
     const [password, setPassword] = useState("");  
     const [passwordError, setPasswordError] = useState("");  
+    const [confirmation, setConfirmation] = useState("");  
+    const [confirmationError, setConfirmationError] = useState("");  
 
     const handleLname = (e) => {
         setLname(e.target.value);
         if(e.target.value.length < 1) {
-            setLnameError("Title is required!");
+            setLnameError("last name is required!");
         } else if(e.target.value.length < 3) {
-            setLnameError("Title must be 3 characters or longer!");
+            setLnameError("Title must be 2 characters or longer!");
         } else {
-            // an empty string is considered a "falsy" value
             setLnameError("");
         }
     }
@@ -26,51 +27,54 @@ const UserForm = ( ) => {
     const handleEmail = (e) => {
         setEmail(e.target.value);
         if(e.target.value.length < 1) {
-            setEmailError("Title is required!");
-        } else if(e.target.value.length < 3) {
-            setEmailError("Title must be 3 characters or longer!");
+            setEmailError("email required!");
+        } else if(e.target.value.length < 5) {
+            setEmailError("Title must be 5 characters or longer!");
         } else {
-            // an empty string is considered a "falsy" value
             setEmailError("");
         }
     }
 
     const handlePassword = (e) => {
         setPassword(e.target.value);
-        if(e.target.value.length < 1) {
-            setPasswordError("Title is required!");
-        } else if(e.target.value.length < 3) {
-            setPasswordError("Title must be 3 characters or longer!");
+        if(e.target.value.length > confirmation) {
+            setPasswordError("Gotta Match!");
+        } else if(e.target.value.length < 8) {
+            setPasswordError("Title must be 8 characters or longer!");
         } else {
-            // an empty string is considered a "falsy" value
             setPasswordError("");
+        }
+    }
+
+    const handleConfirmation = (e) => {
+        setConfirmation(e.target.value);
+        if((e.target.value) > password) {
+            setConfirmationError("gotta match!");
+        } else {
+            setConfirmationError("");
         }
     }
 
     const handleFName = (e) => {
         setFname(e.target.value);
         if(e.target.value.length < 1) {
-            setFnameError("Title is required!");
+            setFnameError("first name!");
         } else if(e.target.value.length < 3) {
-            setFnameError("Title must be 3 characters or longer!");
+            setFnameError("first name must be 3 characters or longer!");
         } else {
-            // an empty string is considered a "falsy" value
             setFnameError("");
         }
     }
 
     const createUser = (e) => {
-        // we must prevent the default refresh of the browser to keep our state from being reset
+        //  prevent the default refresh of the browser
         e.preventDefault();
     
-        // create a javascript object to hold all of the values
+        // create a javascript object to hold all of the values wowza
         const newUser = { fname, email, password};
         console.log("Welcome", newUser);
     };
 
-    
-    
-    {/* this functionality can be repeated for each input needing validation */}
     
     return (
         <form onSubmit={createUser}>
@@ -108,6 +112,15 @@ const UserForm = ( ) => {
                 {
                     passwordError ?
                     <p>{ passwordError }</p> :
+                    ''
+                }
+            </div>
+            <div>
+                <label>Confirm Password: </label>
+                <input type="password" onChange={ handleConfirmation } />
+                {
+                    confirmationError ?
+                    <p>{ confirmationError }</p> :
                     ''
                 }
             </div>
